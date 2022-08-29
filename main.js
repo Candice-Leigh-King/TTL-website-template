d48e6f4335e2a0ffddb7d5c15ee1acdef3eea584
Candice-Leigh-King <candyschofie@gmail.com>
2022-08-29 14:16:04 +0700

    {{item.left.subheading}}

--- main.js
+++ main.js
@@ -77,6 +77,22 @@ $(document).ready(function() {
 				  moreText.style.display = "inline";
 				}
 			  }
+
+			// function myFunction() {
+			// 	var dots = document.getElementById("dots-1");
+			// 	var moreText = document.getElementById("more-1");
+			// 	var btnText = document.getElementById("myBtn-1");
+			  
+			// 	if (dots.style.display === "none") {
+			// 	  dots.style.display = "inline";
+			// 	  btnText.innerHTML = "Read more";
+			// 	  moreText.style.display = "none";
+			// 	} else {
+			// 	  dots.style.display = "none";
+			// 	  btnText.innerHTML = "Read less";
+			// 	  moreText.style.display = "inline";
+			// 	}
+			//   }
  
 			
 
