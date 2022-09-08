1f1d5afbcb5aa3638763efa9e01c85f51cb636b0
Candice-Leigh-King <candyschofie@gmail.com>
2022-09-08 21:26:50 +0700

    d

--- _site/main.js
+++ _site/main.js
@@ -70,28 +70,3 @@ $(document).ready(function() {
 				   $(this).slideToggle();
 				 }});
 			});
-
-			// function myFunction() {
-			// 	var dots = document.getElementById("dots-1");
-			// 	var moreText = document.getElementById("more-1");
-			// 	var btnText = document.getElementById("myBtn-1");
-			  
-			// 	if (dots.style.display === "none") {
-			// 	  dots.style.display = "inline";
-			// 	  btnText.innerHTML = "Read more";
-			// 	  moreText.style.display = "none";
-			// 	} else {
-			// 	  dots.style.display = "none";
-			// 	  btnText.innerHTML = "Read less";
-			// 	  moreText.style.display = "inline";
-			// 	}
-			//   }
- 
-			
-
-
-// 			if (window.inEditorMode) {
-//   alert('Inside CloudCannon!');
-// } else {
-//   alert('Not in CloudCannon.');
-// }
